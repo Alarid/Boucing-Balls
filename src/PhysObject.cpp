8ae4d4481c148dc099c52af95fbbe0f44dc568ce
Krozark <maxime.barbier1991@gmail.com>
2013-10-16 17:24:13 +0200

    add color to direction debug

--- src/PhysObject.cpp
+++ src/PhysObject.cpp
@@ -10,18 +10,12 @@ PhysObject::~PhysObject()
 
 void PhysObject::draw(sf::RenderTarget &target, sf::RenderStates states)const
 {
-    sf::VertexArray lines(sf::LinesStrip, 4);
+    sf::VertexArray lines(sf::Lines, 2);
     lines[0].position = shape->getPosition();
     lines[0].color = sf::Color::Red;
 
     lines[1].position = shape->getPosition() + 8.f*direction;
-    lines[1].color = sf::Color::Red;
-
-    lines[2].position = shape->getPosition();
-    lines[2].color = sf::Color::Red;
-
-    lines[3].position = shape->getPosition() + 8.f*direction;
-    lines[3].color = sf::Color::Red;
+    lines[1].color = sf::Color::Blue;
 
     target.draw(*shape,states);
     target.draw(lines,states);
